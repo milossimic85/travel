90546c555b4d48824ab1c49dde44a7c77374b94d
po_milos_simic <milos.simic@profitoptics.com>
2025-02-09 17:01:41 +0100

    small changes

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
       <div className="absolute z-[100] w-full h-full left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
         <div className="flex items-center w-full h-full justify-center flex-col">
           <div>
-          <h1 className='text-[25px] lg:text-[55px] uppercase tracking-[0.7rem] xl:[45px] mb-4 md:mb-0 text-center font-semibold text-white'>Let&apos;s enjoy the nature!</h1>
+          <h1 className='text-[25px] lg:text-[55px] uppercase tracking-[0.7rem] xl:[45px] mb-4 md:mb-0 text-center font-semibold text-white'>Let&apos;s enjoy the Skiing!</h1>
           <p className='md:text-based text-center font-normal text-lg [word-spacing:5px] text-white'>Get the best prices on 2,000,000+ properties,worldwide</p>
          </div>
          <SearchBox/>
